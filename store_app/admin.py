1f45b10aa2a797e9d341dea4a3ecc57aaa5f19a4
Sibgatullah <sibgatullah881@gmail.com>
2024-10-05 02:30:08 +0600

    created admin pannel

--- store_app/admin.py
+++ store_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Catagory,Customer,Product,Order
 
 # Register your models here.
+
+admin.site.register(Catagory)
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
